9c7a5e9998d38712a621f3087076f458b69ca384
屈小祥 <332381259@qq.com>
2026-04-15 19:01:20 +0800

    11

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,5 @@ import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-	plugins: [
-		react(),
-		// 👇 只修改这里：指定 worker 入口文件
-		cloudflare({
-			worker: {
-				entry: "src/worker/index.ts" // 明确告诉插件：后端在这里！
-			}
-		})
-	],
+	plugins: [react(), cloudflare()],
 });
